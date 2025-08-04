15f0ede508c91b40dcb8d580c478d5fcd53a9e44
yasin <yasinamb.1384amb.11228@gmail.com>
2025-08-04 21:53:05 +0330

    code tile

--- Assets/Scripts/Tiles.cs
+++ Assets/Scripts/Tiles.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tiles : MonoBehaviour
 {
 
+    public GameObject tileGameObjects;
+    public Text numberTileText;
+
+    public void GameDatas(GameObject tileGameObjects, int numberTileText)
+    {
+        this.tileGameObjects = tileGameObjects;
+        this.numberTileText.text = numberTileText.ToString();
+    }
 }
